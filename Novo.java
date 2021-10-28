bfea6a538a8a1dc5ffe76f567091c06c16ca7ac3
RogerioGNunes <rg.nunes2013@gmail.com>
2021-10-28 19:29:04 +0000

    arquivos exerc 5 de herança

---
+++ Novo.java
@@ -0,0 +1,12 @@
+public class Novo extends Imovel{
+
+    private double adicionalPreco;
+
+    public void setAdicionalPreco(double adicionalPreco){
+        this.adicionalPreco=adicionalPreco;
+    }
+    public double getAdicionalPreco(){
+        return this.adicionalPreco;
+    }
+
+}
